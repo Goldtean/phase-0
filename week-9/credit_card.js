162387efdba73c0abd75da14216969a2d57ed90a
Goldtean <jeremyoltean@gmail.com>
2016-02-02 11:01:21 -0700

    Fix 9.2

--- week-9/credit_card.js
+++ week-9/credit_card.js
@@ -12,6 +12,9 @@
 // is the same as doubling every second number starting from the first number.
 // First, I will likely turn the string of numbers into an array so they can be more easily modified.
 // Then I will do something like a FOR loop or other iterative technique to double all appropriate numbers.
+// I will put all the doubled numbers and the non-doubled numbers into an array. Since I want to add up
+// all the single digits (eg. 3 + 10 + 15 = 3 + 1 + 0 + 1 + 5) I will turn this array into a string with no
+// separators then turn that into an array with each number separated. Then I will add up that array.
 // Lastly, I will use the trusty old modulus to see if it's divisible by 10 and use an IF statement
 // to accept or decline the card.
 // Because the challenge isn't clear and we aren't supposed to look back at our answers, I am assuming the card
@@ -52,7 +55,10 @@
 // Refactored solution
 
 // Looking back it appears that the number was passed through as a number, so I have altered the validate
-// function to first string-ify the number.
+// function to first string-ify the number. Also, I missed the last two steps and only realized after my
+// initial solution. Instead of adding up all the numbers, I was only adding the doubled numbers, and I
+// also wasn't splitting them correctly. That's all been fixed for the refactor, along with a fancy
+// shmansy reduce method for summing an array in one line.
 
 var card_number = 4408041234567901
 
@@ -64,12 +70,17 @@ function validate(card) {
 		return false;
 	}
 	else {
-		var double_me_up = []
+		var string_me = []
 			for (var i = 0; i < card_array.length; i = i + 2) {
-				double_me_up.push(card_array[i] * 2);
+				string_me.push(card_array[i] * 2);
 			}
+			for (var i = 1; i < card_array.length; i = i + 2) {
+				string_me.push(card_array[i]);
+			}
+			var array_me = string_me.join("");
+			var count_me = array_me.split("").map(Number);
 
-			var count = double_me_up.reduce((x, y) => x + y);
+			var count = count_me.reduce((x, y) => x + y);
 
 			if (count % 10 == 0) {
 				console.log("Transaction approved!");
